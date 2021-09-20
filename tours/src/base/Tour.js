05053de4a216accd931fca4e4016ad72d7248851
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-20 17:07:22 -0400

    Tours - Toggle Info

--- tours/src/base/Tour.js
+++ tours/src/base/Tour.js
@@ -1,20 +1,34 @@
 import { string } from 'prop-types';
+import { useState } from 'react/cjs/react.development';
 
-const Tour = ({ name, info, image, price }) => (
-  <article className="single-tour">
-    <img src={image} alt={name} />
-    <footer>
-      <section className="tour-info">
-        <h4>{name}</h4>
-        <h4 className="tour-price">{price}</h4>
-      </section>
-      <p>{info}</p>
-      <button type="button" className="delete-btn">
-        Sem interesse
-      </button>
-    </footer>
-  </article>
-);
+const Tour = ({ name, info, image, price }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const handleClickReadMore = () => {
+    setReadMore((prev) => !prev);
+  };
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <section className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </section>
+        <p>
+          {readMore ? info : `${info.slice(0, 200)}...`}{' '}
+          <button type="button" onClick={handleClickReadMore}>
+            {readMore ? 'Ler menos' : 'Ler mais'}
+          </button>{' '}
+        </p>
+        <button type="button" className="delete-btn">
+          Sem interesse
+        </button>
+      </footer>
+    </article>
+  );
+};
 
 Tour.propTypes = {
   name: string.isRequired,
