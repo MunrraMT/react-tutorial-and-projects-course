eda3dc4feddcd717584190884e4dc4c1b320eb01
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-21 13:51:09 -0400

    Initial Setup

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const url = 'https://course-api.com/react-tabs-project';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>{url}</h1>
+    </main>
   );
 }
 
