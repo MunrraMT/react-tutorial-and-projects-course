9c92c264e7f6229bb57309a4606b986fbfaf8318
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-21 12:42:44 -0400

    Accordion - Complete

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+
+import data from './base/data';
+import Question from './base/Question';
+
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <main>
-      <h1>Teste</h1>
+      <section className="container">
+        <h3>Perguntas e respostas sobre login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <Question
+              key={question.id}
+              title={question.title}
+              info={question.info}
+            />
+          ))}
+        </section>
+      </section>
     </main>
   );
 }
--- accordion/src/base/Question.js
+++ accordion/src/base/Question.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
+import { string } from 'prop-types';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleClickShowInfo = () => {
+    setShowInfo((prev) => !prev);
+  };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" type="button" onClick={handleClickShowInfo}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+Question.propTypes = {
+  title: string.isRequired,
+  info: string.isRequired,
 };
 
 export default Question;
