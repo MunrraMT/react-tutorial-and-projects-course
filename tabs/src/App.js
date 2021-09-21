daa5ae4102478cfc42187ddc04904678a306397a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-21 16:03:39 -0400

    Tabs - Fetch Jobs

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,9 +1,37 @@
+/* eslint-disable */
+import { useEffect, useState } from 'react';
+
 function App() {
-  const url = 'https://course-api.com/react-tabs-project';
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const url = 'https://course-api.com/react-tabs-project';
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
   return (
-    <main className="App">
-      <h1>{url}</h1>
+    <main>
+      {loading && (
+        <section className="section loading">
+          <h1>Loading...</h1>
+        </section>
+      )}
+
+      {!loading && (
+        <section className="section">
+          <h1>teste</h1>
+        </section>
+      )}
     </main>
   );
 }
