d2edae303c19291fcb1f0d29950efc893a224962
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-22 14:52:48 -0400

    Lorem Ipsum - Structure

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,8 +1,46 @@
+/* eslint-disable */
+import { useEffect, useState } from 'react';
+import data from './base/data';
+
 function App() {
+  const [textBase, setTextBase] = useState([]);
+  const [count, setCount] = useState(0);
+  const [textGenerated, setTextGenerated] = useState([]);
+
+  useEffect(() => {
+    setTextBase(data);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(count);
+  };
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
   return (
-    <main>
-      <h1>Teste</h1>
-    </main>
+    <section className="section-center">
+      <h3>cansado de lorem ipsum chato?</h3>
+
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Parágrafos:</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn">
+          gerar
+        </button>
+      </form>
+
+      <article className="lorem-text">{textGenerated}</article>
+    </section>
   );
 }
 
