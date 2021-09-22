63e2598e437e39be1812c698f43f6432f1abfb60
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-22 13:06:23 -0400

    Slider - Setup Classes

--- slider/src/App.js
+++ slider/src/App.js
@@ -14,6 +14,16 @@ function App() {
     setPeople(data);
   }, []);
 
+  const articleFormatted = (position, { id, image, name, title, quote }) => (
+    <article className={position} key={id}>
+      <img src={image} alt={name} className="person-img" />
+      <h4>{name}</h4>
+      <p className="title">{title}</p>
+      <p className="text">{quote}</p>
+      <FaQuoteRight className="icon" />
+    </article>
+  );
+
   return (
     <main>
       <section className="section">
@@ -25,16 +35,19 @@ function App() {
 
         <section className="section-center">
           {people.length > 0 &&
-            people.map(({ id, image, name, title, quote }, personIndex) => {
-              return (
-                <article key={id}>
-                  <img src={image} alt={name} className="person-img" />
-                  <h4>{name}</h4>
-                  <p className="title">{title}</p>
-                  <p className="text">{quote}</p>
-                  <FaQuoteRight className="icon" />
-                </article>
-              );
+            people.map((person, personIndex) => {
+              if (personIndex === index) {
+                return articleFormatted('activeSlide', person);
+              }
+
+              if (
+                personIndex === index - 1 ||
+                (index === 0 && personIndex === people.length - 1)
+              ) {
+                return articleFormatted('lastSlide', person);
+              }
+
+              return articleFormatted('nextSlide', person);
             })}
 
           <button type="button" className="prev">
