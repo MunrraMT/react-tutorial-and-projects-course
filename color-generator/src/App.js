33ddaecfbe7f5b3b9c56365a217edc17c755aaf7
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-07 10:32:14 -0400

    Color Generator - Structure

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,69 +1,40 @@
-/* eslint-disable */
-import { useEffect, useState } from 'react';
-import data from './base/data';
+import { useState } from 'react';
 
 function App() {
-  const [textBase, setTextBase] = useState([]);
-  const [count, setCount] = useState('0');
-  const [textGenerated, setTextGenerated] = useState([]);
-
-  useEffect(() => {
-    setTextBase(data);
-  }, []);
-
-  const randomNumber = () => Math.floor(Math.random() * 100000);
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (textBase.length === 0)
-      setTextGenerated('Servidor de texto não encontrado.');
-
-    if (Number(count) === 0) {
-      setTextGenerated(
-        textBase.map((text) => <p key={randomNumber()}>{text}</p>),
-      );
-    }
-
-    if (Number(count) > 0) {
-      setTextGenerated(
-        textBase
-          .slice(0, Number(count))
-          .map((text) => <p key={randomNumber()}>{text}</p>),
-      );
-    }
+    console.log('Hello');
   };
 
   const handleChange = (e) => {
-    setCount(
-      e.target.value < 0
-        ? 0
-        : e.target.value > textBase.length
-        ? textBase.length
-        : e.target.value,
-    );
+    setColor(e.target.value);
   };
 
   return (
-    <section className="section-center">
-      <h3>cansado de lorem ipsum chato?</h3>
+    <>
+      <section className="container">
+        <h1>color generator</h1>
 
-      <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">Parágrafos:</label>
-        <input
-          type="number"
-          id="amount"
-          name="amount"
-          value={count}
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn">
-          gerar
-        </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={handleChange}
+          />
+          <button type="submit">submit</button>
+        </form>
+      </section>
 
-      <article className="lorem-text">{textGenerated}</article>
-    </section>
+      <section className="colors">
+        <h2>list goes here</h2>
+      </section>
+    </>
   );
 }
 
