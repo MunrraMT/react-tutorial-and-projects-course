5dc388841273f5d3e172b2b612667e06dd3a23f5
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-07 16:47:50 -0400

    Navbar - Data

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './base/Navbar';
+import Sidebar from './base/Sidebar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Sidebar />
     </>
   );
 }
--- navbar/src/base/Navbar.js
+++ navbar/src/base/Navbar.js
@@ -1,59 +1,37 @@
-/* eslint-disable */
-import { useState, useRef, useEffect } from 'react';
-
-import { FaBars, FaTwitter } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 
 import { links, social } from './data';
 import logo from './logo.svg';
 
-const Navbar = () => {
-  return (
-    <nav>
-      <section className="nav-center">
-        <header className="nav-header">
-          <img src={logo} alt="logo" />
-          <button type="button" className="nav-toggle">
-            <FaBars />
-          </button>
-        </header>
+const Navbar = () => (
+  <nav>
+    <section className="nav-center">
+      <header className="nav-header">
+        <img src={logo} alt="logo" />
+        <button type="button" className="nav-toggle">
+          <FaBars />
+        </button>
+      </header>
 
-        <section className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="#">home</a>
+      <section className="links-container show-container">
+        <ul className="links">
+          {links.map((link) => (
+            <li key={link.id}>
+              <a href={link.url}>{link.text}</a>
             </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
-          </ul>
-        </section>
-
-        <ul className="social-icons">
-          <li>
-            <a href="#">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaTwitter />
-            </a>
-          </li>
+          ))}
         </ul>
       </section>
-    </nav>
-  );
-};
+
+      <ul className="social-icons">
+        {social.map((item) => (
+          <li key={item.id}>
+            <a href={item.url}>{item.icon}</a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  </nav>
+);
 
 export default Navbar;
