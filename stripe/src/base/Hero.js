05d97c8bb24180218fe0f7b1d817bf2cbb8ad6ed
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-13 17:08:30 -0400

    Stripe - Hero

--- stripe/src/base/Hero.js
+++ stripe/src/base/Hero.js
@@ -1,13 +1,34 @@
-// import phoneImg from './images/phone.svg'
+import phoneImg from './images/phone.svg';
 
 import { useGlobalContext } from './Context';
 
 const Hero = () => {
-  const data = useGlobalContext();
+  const { toggleSubmenuOpen } = useGlobalContext();
 
-  console.dir(data);
+  return (
+    <section className="hero">
+      <section className="hero-center">
+        <article className="hero-info">
+          <h1>Infraestrutura de pagamentos para a internet</h1>
 
-  return <h2>hero component</h2>;
+          <p>
+            Milhões de empresas de todos os tamanhos, de pequenas startups a
+            grandes corporações, usam soluções de software e API da Stripe para
+            receber pagamentos, enviar repasses e gerenciar suas operações
+            online.{' '}
+          </p>
+
+          <button type="button" className="btn">
+            Comece agora
+          </button>
+        </article>
+
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </section>
+    </section>
+  );
 };
 
 export default Hero;
