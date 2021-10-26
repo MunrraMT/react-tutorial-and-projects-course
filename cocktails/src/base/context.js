54140f95266d975154e26604fc0c72d4e29b62b6
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-25 23:45:01 -0400

    Cocktails - Setup Overview

--- cocktails/src/base/context.js
+++ cocktails/src/base/context.js
@@ -1,15 +1,18 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import { node } from 'prop-types';
+import { createContext, useContext } from 'react';
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+// const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+const AppContext = createContext();
 
-export { AppContext, AppProvider }
+const AppProvider = ({ children }) => (
+  <AppContext.Provider value="hello">{children}</AppContext.Provider>
+);
+
+AppProvider.propTypes = {
+  children: node.isRequired,
+};
+
+const useGlobalContext = () => useContext(AppContext);
+
+export { AppProvider, useGlobalContext };
