1904e160f483f4be7a6f467a4c564c7af164d406
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-26 00:22:53 -0400

    Cocktails - About and Error

--- cocktails/src/base/pages/About.js
+++ cocktails/src/base/pages/About.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
+const About = () => (
+  <section className="section about-section">
+    <h1 className="section-title">about us</h1>
 
-const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  );
-};
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+      reprehenderit laborum numquam sed aspernatur labore error beatae deserunt
+      repellendus, voluptas minima, porro praesentium tempore, sint ea vitae
+      aperiam commodi possimus maxime! Sint omnis vero nostrum, vitae eaque
+      blanditiis? Recusandae, sunt!
+    </p>
+  </section>
+);
 
 export default About;
--- cocktails/src/base/pages/Error.js
+++ cocktails/src/base/pages/Error.js
@@ -1,13 +1,15 @@
-/* eslint-disable */
-
 import { Link } from 'react-router-dom';
 
-const Error = () => {
-  return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  );
-};
+const Error = () => (
+  <section className="section error-page">
+    <section className="error-container">
+      <h1>oops! it&rsquo;s a dead end</h1>
+
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
+    </section>
+  </section>
+);
 
 export default Error;
