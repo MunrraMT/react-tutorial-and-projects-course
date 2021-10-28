3170f97ab6e7ebea456397582b8778c105712ffe
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-28 11:37:14 -0400

    Create-React-App && ESLint/Prettier && Initial Setup

---
+++ secao-6/pagination/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import { useState, useEffect } from 'react';
+
+import { useFetch } from './useFetch';
+import Follower from './Follower';
+
+function App() {
+  return <h2>pagination starter</h2>;
+}
+
+export default App;
