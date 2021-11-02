36fb657c1b5f5ae37facb1a93ef8bca12d7952ed
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-11-01 20:07:56 -0400

    Stock Photos - Initial Fetch

--- secao-6/stock-photos/src/App.js
+++ secao-6/stock-photos/src/App.js
@@ -5,12 +5,37 @@ import { FaSearch } from 'react-icons/fa';
 
 import Photo from './base/Photo';
 
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 
-const mainUrl = `https://api.unsplash.com/photos/`;
-const searchUrl = `https://api.unsplash.com/search/photos/`;
+const baseUrl = `https://api.unsplash.com/`;
+const mainEndPoint = `/photos/`;
+const searchEndPoint = `/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async (params = '') => {
+    setLoading(true);
+
+    try {
+      const url = `${baseUrl}${params}?client_id=JS5gT-dtNwkEZQ4swt3cspvFm8VdVruRAP_IMlJSxk4`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages(mainEndPoint);
+  }, []);
+
   return <h2>stock photos starter</h2>;
 }
 
