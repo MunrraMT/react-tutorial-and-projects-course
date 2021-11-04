dddb0fa626efd290e562c584fd266571b1272b0a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-11-04 16:45:22 -0400

    Dark Mode - Setup

--- secao-6/dark-mode/src/App.js
+++ secao-6/dark-mode/src/App.js
@@ -1,8 +1,31 @@
-// import data from './data'
-// import Article from './Article'
+import data from './base/data';
+import Article from './base/Article';
 
 function App() {
-  return <h2>dark mode starter</h2>;
+  return (
+    <main>
+      <nav>
+        <section className="nav-center">
+          <h1>overreacted</h1>
+          <button type="button" className="btn">
+            toggle
+          </button>
+        </section>
+      </nav>
+
+      <section className="articles">
+        {data.map((item) => (
+          <Article
+            key={item.id}
+            title={item.title}
+            snippet={item.snippet}
+            date={item.date}
+            length={item.length}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- secao-6/dark-mode/src/base/Article.js
+++ secao-6/dark-mode/src/base/Article.js
@@ -1,5 +1,24 @@
+/* eslint-disable no-unused-vars */
 // import moment from 'moment'
 
-const Article = () => <h2>article component</h2>;
+import { instanceOf, number, string } from 'prop-types';
+
+const Article = ({ title, snippet, date, length }) => (
+  <article className="post">
+    <h2>{title}</h2>
+    <footer className="post-info">
+      <span>{date.toLocaleDateString()}</span>
+      <span>{length} min read</span>
+    </footer>
+    <p>{snippet}</p>
+  </article>
+);
+
+Article.propTypes = {
+  title: string.isRequired,
+  snippet: string.isRequired,
+  date: instanceOf(Date).isRequired,
+  length: number.isRequired,
+};
 
 export default Article;
