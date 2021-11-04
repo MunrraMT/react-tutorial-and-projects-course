64e38d239521c4e88c2396cd71f2d9a18fc6869f
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-11-04 16:26:37 -0400

    Create-React-App && ESLint/Prettier && Initial Setup

---
+++ secao-6/dark-mode/src/App.js
@@ -0,0 +1,8 @@
+// import data from './data'
+// import Article from './Article'
+
+function App() {
+  return <h2>dark mode starter</h2>;
+}
+
+export default App;
---
+++ secao-6/dark-mode/src/base/Article.js
@@ -0,0 +1,5 @@
+// import moment from 'moment'
+
+const Article = () => <h2>article component</h2>;
+
+export default Article;
