3f87b0f9bb87fa47111a37a7bad42ef8436f6407
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-11-12 16:57:27 -0400

    Movie DB - Movies Component

--- secao-6/movie-db/src/base/Movies.js
+++ secao-6/movie-db/src/base/Movies.js
@@ -1,16 +1,35 @@
 /* eslint-disable no-unused-vars */
 
-// import { Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import { useGlobalContext } from './context';
 
 const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 
-const Movies = () => {
-  const data = useGlobalContext();
+const noImage = './No_image_available.svg';
 
-  return <h2>movies component</h2>;
+const Movies = () => {
+  const { movies, isLoading } = useGlobalContext();
+
+  if (isLoading) return <section className="loading" />;
+
+  return (
+    <section className="movies">
+      {movies.map(
+        ({ imdbID: id, Poster: poster, Title: title, Year: year }) => (
+          <Link to={`/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === 'N/A' ? noImage : poster} alt={title} />
+              <footer className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </footer>
+            </article>
+          </Link>
+        ),
+      )}
+    </section>
+  );
 };
 
 export default Movies;
