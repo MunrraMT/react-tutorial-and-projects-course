002ff2e829fa87b77a6b627af71cdb14d77cccde
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-01-06 21:46:35 -0400

    227. Hacker News - Display Stories

--- secao-6/hacker-news/src/base/Stories.js
+++ secao-6/hacker-news/src/base/Stories.js
@@ -1,9 +1,29 @@
+/* eslint-disable  */
 import { useGlobalContext } from './context';
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
 
-  return isLoading ? <div className="loading" /> : <h2>stories component</h2>;
+  return isLoading ? (
+    <div className="loading" />
+  ) : (
+    <section className="stories">
+      {hits.map(({ objectID, title, num_comments, url, points, author }) => (
+        <article key={objectID} className="story">
+          <h4 className="title">{title}</h4>
+          <p className="info">
+            {points} points by <span>{author} |</span> {num_comments} comments
+          </p>
+          <div>
+            <a href={url} className="read-link" target="_blank">
+              read more
+            </a>
+            <button className="remove-btn">remove</button>
+          </div>
+        </article>
+      ))}
+    </section>
+  );
 };
 
 export default Stories;
