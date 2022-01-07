4588bb548ac12615ddd50a2263ed74e767689b4c
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-01-06 21:32:23 -0400

    226. Hacker News - Fetch Stories

--- secao-6/hacker-news/src/base/context.js
+++ secao-6/hacker-news/src/base/context.js
@@ -1,13 +1,17 @@
 import { useContext, createContext, useReducer, useEffect } from 'react';
 import { node } from 'prop-types';
 
-import { SET_LOADING } from './actions';
+import { SET_LOADING, SET_STORIES } from './actions';
 import reducer from './reducer';
 
-// const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
 
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: 'kant',
+  page: 0,
+  numberPages: 0,
 };
 
 const AppContext = createContext();
@@ -15,12 +19,23 @@ const AppContext = createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchStories = async () => {
+  const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING });
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, []);
 
   return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
