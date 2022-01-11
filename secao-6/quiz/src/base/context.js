d2eb0253cd25c8b4a236e47e8bcc165736ad233a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-01-11 17:12:10 -0400

    235. Quiz - Fetch Questions

--- secao-6/quiz/src/base/context.js
+++ secao-6/quiz/src/base/context.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
-import { useContext, createContext, useState } from 'react';
+import { useContext, createContext, useState, useEffect } from 'react';
 import { node } from 'prop-types';
+import axios from 'axios';
 
 // const table = {
 //   sports: 21,
@@ -10,7 +11,8 @@ import { node } from 'prop-types';
 // }
 
 // const API_ENDPOINT = 'https://opentdb.com/api.php?'
-// const tempUrl = https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple';
 
 // const url = ''
 
@@ -24,6 +26,33 @@ const AppProvider = ({ children }) => {
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+
+    const response = await axios(url).catch((err) => console.log(err));
+
+    if (response) {
+      const data = await response.data.results;
+
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
 
   return (
     <AppContext.Provider
