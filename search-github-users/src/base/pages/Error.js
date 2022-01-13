531a45cfa870b12854bb5e7a021496a8a1a213d8
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-01-13 14:15:46 -0400

    250. Error Page

--- search-github-users/src/base/pages/Error.js
+++ search-github-users/src/base/pages/Error.js
@@ -1,21 +1,34 @@
-// import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Error = () => <h2>error page</h2>;
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+  .btn {
+    max-width: 10rem;
+  }
+`;
 
-// const Wrapper = styled.section`
-//   min-height: 100vh;
-//   display: grid;
-//   place-items: center;
-//   background: var(--clr-primary-10);
-//   text-align: center;
-//   h1 {
-//     font-size: 10rem;
-//   }
-//   h3 {
-//     color: var(--clr-grey-3);
-//     margin-bottom: 1.5rem;
-//   }
-// `;
+const Error = () => (
+  <Wrapper>
+    <h1>404</h1>
+    <h3>sorry, the page you tried cannot be found</h3>
+    <Link to="/" className="btn">
+      back page
+    </Link>
+  </Wrapper>
+);
 
 export default Error;
