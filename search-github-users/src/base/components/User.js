5ed947104a73608fa1f1bcb7ade57345d5d8d6d3
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-02-07 21:52:54 -0400

    255. Card Component

--- search-github-users/src/base/components/User.js
+++ search-github-users/src/base/components/User.js
@@ -1,17 +1,24 @@
-// import styled from 'styled-components';
-// import Card from './Card';
-// import Followers from './Followers';
+import styled from 'styled-components';
+import Card from './Card';
+import Followers from './Followers';
 
-const User = () => <h2>search component</h2>;
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  /* align-items: start; */
+`;
 
-// const Wrapper = styled.div`
-//   padding-top: 2rem;
-//   display: grid;
-//   gap: 3rem 2rem;
-//   @media (min-width: 992px) {
-//     grid-template-columns: 1fr 1fr;
-//   }
-//   /* align-items: start; */
-// `;
+const User = () => (
+  <section className="section">
+    <Wrapper className="section-center">
+      <Card />
+      <Followers />
+    </Wrapper>
+  </section>
+);
 
 export default User;
