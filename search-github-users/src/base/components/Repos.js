4ad96010e3f302f7ad190fa568ba4fce572d2ea9
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-02-08 10:00:42 -0400

    257. Repos Setup

--- search-github-users/src/base/components/Repos.js
+++ search-github-users/src/base/components/Repos.js
@@ -1,31 +1,36 @@
-// import styled from 'styled-components';
-// import { GithubContext } from '../context/context';
-// import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import styled from 'styled-components';
+import { useGithubContext } from '../context/context';
 
-const Repos = () => <h2>repos component</h2>;
+const Repos = () => {
+  const { githubRepos } = useGithubContext();
 
-// const Wrapper = styled.div`
-//   display: grid;
-//   justify-items: center;
-//   gap: 2rem;
-//   @media (min-width: 800px) {
-//     grid-template-columns: 1fr 1fr;
-//   }
+  console.log(githubRepos);
 
-//   @media (min-width: 1200px) {
-//     grid-template-columns: 2fr 3fr;
-//   }
+  return <Wrapper>repos component</Wrapper>;
+};
 
-//   div {
-//     width: 100% !important;
-//   }
-//   .fusioncharts-container {
-//     width: 100% !important;
-//   }
-//   svg {
-//     width: 100% !important;
-//     border-radius: var(--radius) !important;
-//   }
-// `;
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
 
 export default Repos;
