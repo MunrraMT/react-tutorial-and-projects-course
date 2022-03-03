8ef1228f4f5c663856d9b0384c4255cac1b9425c
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-03-03 14:44:33 -0400

    293. Footer

--- ecommerce-project/src/base/components/Footer.js
+++ ecommerce-project/src/base/components/Footer.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
-const Footer = () => <Wrapper>footer</Wrapper>;
+const Footer = () => (
+  <Wrapper>
+    <h5>
+      &copy; {new Date().getFullYear()}
+      <span>ComfySloth</span>
+    </h5>
+    <h5>All rights reserved</h5>
+  </Wrapper>
+);
 
 const Wrapper = styled.footer`
   height: 5rem;
