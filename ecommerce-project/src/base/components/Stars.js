fc31a5fd107a9e3181e9d6d94c74e7bd351dff7f
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-03-11 07:49:19 -0400

    314. Single Product - Stars (array approach)

--- ecommerce-project/src/base/components/Stars.js
+++ ecommerce-project/src/base/components/Stars.js
@@ -1,7 +1,47 @@
+/* eslint-disable react/no-array-index-key */
+import { number } from 'prop-types';
 import styled from 'styled-components';
-// import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs';
 
-const Stars = () => <Wrapper>stars</Wrapper>;
+const Stars = ({ stars, reviews }) => {
+  const starsIcons = Array.from({ length: 5 }, (__, index) => {
+    const starIndex = index + 1;
+
+    if (starIndex - stars >= 1) {
+      return (
+        <span key={`star${index}`}>
+          <BsStar />
+        </span>
+      );
+    }
+
+    if (starIndex - stars > 0) {
+      return (
+        <span key={`star${index}`}>
+          <BsStarHalf />
+        </span>
+      );
+    }
+
+    return (
+      <span key={`star${index}`}>
+        <BsStarFill />
+      </span>
+    );
+  });
+
+  return (
+    <Wrapper>
+      <div className="stars">{starsIcons}</div>
+      <p className="reviews">{reviews} customer reviews</p>
+    </Wrapper>
+  );
+};
+
+Stars.propTypes = {
+  stars: number.isRequired,
+  reviews: number.isRequired,
+};
 
 const Wrapper = styled.div`
   display: flex;
